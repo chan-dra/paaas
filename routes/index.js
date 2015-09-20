95ee676eb434dec7ecad76ba1427a7b05ed88df9
Chandra Bhavanasi <chandra.bhavanasi@isocket.com>
2015-09-19 17:35:56 -0700

    add xml

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,15 @@
+var xml = require('xml');
 module.exports = function (app) {
     app.get('/', index);
 };
 
 var index = function (req, res) {
-    res.render('index', { title: 'Node Boilerplate' });
+    res.header('Content-Type','text/xml');
+    a = xml({
+    	Response: [{
+    		Say: [{ _attr: { voice: 'alice'}}, "This is a test message"]
+    	}]
+    })
+    console.log(a);
+    res.send(a);
 };
